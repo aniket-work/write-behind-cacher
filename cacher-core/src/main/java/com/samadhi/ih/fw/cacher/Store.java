3e57e5f7dad16d93005a93dc60f3eb1dd97033cc
worka <work.aniket.hingane@gmail.com>
2022-03-27 11:49:14 -0400

    Intial draft

--- cacher-core/src/main/java/com/samadhi/ih/fw/cacher/Store.java
+++ cacher-core/src/main/java/com/samadhi/ih/fw/cacher/Store.java
@@ -8,37 +8,39 @@ import java.util.Optional;
 import java.util.Set;
 
 /**
- *  This is single most contract as how to define a cache or say store
+ * This is single most contract as how to define a cache or say store
+ *
  * @param <K>
  * @param <V>
  */
-public interface Store <K,V> extends AutoCloseable {
+public interface Store<K, V> extends AutoCloseable {
 
     Logger logger = LoggerFactory.getLogger(Store.class); //Note : we cant use lombok on interface as, lombok will make this logger as private which you cant do on interface
 
 
     /**
      * Design choice : if there is no entry and a storeFiller is in place, this method will attempt to fill cache
+     *
      * @param key
      * @return
      */
-    default Optional<V> get (K key) {
+    default Optional<V> get(K key) {
         var singleStoreResultManager = getStoreEntry(key);
 
         if (singleStoreResultManager.isSuccess()) {
             return Optional.of(singleStoreResultManager.getData());
-        }
-        else {
+        } else {
             return Optional.empty();
         }
     }
 
-    SingleStoreResultManager<V>     getStoreEntry(K key);  //Nothing but : CacheGetResult<V> GET(K key);
-    MultiStoreResultManager<K, V>   getStoreEntries(Set<? extends K> keys);  //Nothing but : MultiGetResult<K, V> GET_ALL(Set<? extends K> keys);
+    SingleStoreResultManager<V> getStoreEntry(K key);  //Nothing but : CacheGetResult<V> GET(K key);
 
-    default Map<K, V> getAll(Set<? extends K> keys){
+    MultiStoreResultManager<K, V> getStoreEntries(Set<? extends K> keys);  //Nothing but : MultiGetResult<K, V> GET_ALL(Set<? extends K> keys);
+
+    default Map<K, V> getAll(Set<? extends K> keys) {
         var storeEntries = getStoreEntries(keys);
-
+        return null;
     }
 
 }
